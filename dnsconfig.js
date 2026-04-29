ea9b8351ec2a9c7f25afa4b49d9b3ff4439142fd
shuvo Mukherjee <shuvo@kambaii.com>
2026-04-29 20:17:47 +0600

    Update DNS Map with fresh Cloudflare IPs

--- dnsconfig.js
+++ dnsconfig.js
@@ -5,7 +5,7 @@ export const setDirectApi = (val) => { directApi = val; };
 export const dnsMap = {
     "api.ivacbd.com": [
         "13.204.113.252",
-         "65.0.229.15",
-        "3.109.20.63"
+         "13.234.148.99",
+         "13.126.63.149"
     ]
 };
